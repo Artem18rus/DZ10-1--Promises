647577c751fe128144e0a2311d22949ae44d0db6
Artem18 <urart88@mail.ru>
2022-10-02 16:46:57 +0400

    DZ10-1--Promises

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,12 +1,8 @@
-import json from '../parser.js';
-import read from '../reader.js';
-import GameSavingLoader from '../app.js';
+/* eslint-disable linebreak-style */
+import GameSavingLoader from '../app';
 
-//jest.setTimeout(15000);
 test('Function-OK1', async (done) => {
   const data = await GameSavingLoader.load();
   expect(data).toEqual('{id: 9, created: 1546300800, userInfo: { id: 1, name: "Hitman", level: 10, points: 2000}}');
   done();
 });
-
-
